98710acd85a29557bddf1fc2013dfa43e555f12a
Stanislav Dombrovsky <stanislav.dombrovsky@jetbrains.com>
2020-03-31 15:29:22 +0300

    Revert all

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import ios_test_projectsTests
-
-var tests = [XCTestCaseEntry]()
-tests += ios_test_projectsTests.allTests()
-XCTMain(tests)
--- Tests/tests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(ios_test_projectsTests.allTests),
-    ]
-}
-#endif
